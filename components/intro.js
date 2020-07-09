efae65558adb85ff635cb607cf5de1b32085095c
Derrik Campbell <derrikcampbell@hotmail.com>
2020-07-09 16:56:10 -0400

    Added layout design elements and updated mailing option

--- components/intro.js
+++ components/intro.js
@@ -1,14 +1,25 @@
+import Button from 'react-bootstrap/Button'
 
 export default function Intro() {
+
   return (
     
   
-    <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
-
-      <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-      Hi, my name is Derrik Campbell. I'm a Digital Designer.
+    <section className="flex-col md:flex-row flex items-center md:justify-between mt-32 mb-16 md:mb-12">
+      <div>
+      <h1 className="text-5xl md:text-6xl font-bold tracking-tighter leading-tight md:pr-8">
+      Hi, my name is Derrik Campbell. 
       </h1>
-      
+      <h2 className="text-black-50">
+      I am a passionate Visual|designer.
+      <br/>
+      Currently Freelancing.
+      </h2>
+      <br/>
+      <Button variant="outline-dark" size="sm">Learn More</Button>
+      </div>
+    
+
       {/* <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
       I am a passionate Visual|designer. {' '}
       Currently Freelancing and building a portfolio site using Next.js. Here's my current portfolio: 
--- components/post-preview.js
+++ components/post-preview.js
@@ -1,7 +1,6 @@
 import Avatar from './avatar'
 import DateFormater from './date-formater'
 import CoverImage from './cover-image'
-import Link from 'next/link'
 
 export default function PostPreview({
   title,
@@ -13,18 +12,16 @@ export default function PostPreview({
 }) {
   return (
     <div>
-      <div className="mb-5 rounded">
+      <div className="mb-4 rounded">
         <CoverImage slug={slug} title={title} src={coverImage} />
       </div>
-      <h3 className="text-3xl mb-3 leading-snug">
-        <Link as={`/posts/${slug}`} href="/posts/[slug]">
-          <a className="hover:underline">{title}</a>
-        </Link>
+      <h3 className="text-lg mb-2">
+          <a className="text-dark">{title}</a>
       </h3>
-      <div className="text-lg mb-4">
+      <div className="text-lg mb-2">
         {/* <DateFormater dateString={date} /> */}
       </div>
-      <p className="text-lg leading-relaxed mb-4">{excerpt}</p>
+      <p className="text-lg text-black-50 text-secondary leading-relaxed mb-4">{excerpt}</p>
     </div>
   )
 }
