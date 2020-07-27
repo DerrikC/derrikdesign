6793d343513c67d9e5a3892f5fd2b9af0e87df68
Derrik Campbell <derrikcampbell@hotmail.com>
2020-07-27 14:29:40 -0400

    Updated About page content

--- pages/about.js
+++ pages/about.js
@@ -22,11 +22,12 @@ const about = () => (
         <div className="flex md:flex-row-reverse flex-wrap">
        
         <div className="w-full md:w-2/3 p-1">
-        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <p>I’m a graduate from <a href="https://www.fanshawec.ca/programs/idp3-interactive-media-design/next">Fanshawe’s Interactive Media Design </a> program with a <a href="https://sociology.uwo.ca/undergraduate/career/criminology.html"> B.A. in Criminology</a>. I combine my social science knowledge and design background and apply it to the world of empathetic user-centered design. Over the years my passion for design and technology has motivated me to create refined and practical design experiences.</p>
+        <br></br>
         <br></br>
         <h2 className="text-5xl md:text-6xl font-bold tracking-tighter leading-tight md:pr-8">Let's get Working</h2>
         <br></br>
-        <p>Just send me a message at <a href="">derrikdesign@gmail.com</a>, connect on <a href="">LinkedIn</a>, or checkout my <a href="">Dribbble</a>!</p>
+        <p>Interested in working with me? Just contact me at <a href="https://mail.google.com/mail/u/0/?view=cm&fs=1&tf=1&source=mailto&to=derrikdesign@gmail.com">derrikdesign@gmail.com</a>, or connect on <a href="https://www.linkedin.com/in/derrikcampbell/">LinkedIn</a>!</p>
         </div>
         <div className="w-full md:w-1/3 pt-2 pr-10">
         
